4bfe53984a4e74caecc66e1026392ddbb5ddc239
dongduy956 <dongduy0612@gmail.com>
2021-03-12 22:38:40 +0700

    add about

---
+++ assets/js/about-us.js
@@ -0,0 +1,36 @@
+var icon=document.querySelector('.container__boat-img')
+var video=document.querySelector('.container__group-video')
+var iconclose=document.querySelector('.container__icon-close')
+console.log(iconclose)
+var overlay=document.querySelector('.overlay')
+icon.onclick=function(){
+   Object.assign(overlay.style,{
+       opacity:1,
+       zIndex:1,
+       transition:'all .5s linear'
+   })
+   Object.assign(video.style,{
+       zIndex:1,
+       opacity:1
+   })
+}
+var functionClose=function(){
+    Object.assign(overlay.style,{
+        opacity:0,
+        zIndex:-1,
+        transition:'all .7s linear'
+    })
+    Object.assign(video.style,{
+        zIndex:-1,
+        opacity:0
+    })
+}
+overlay.onclick=function(){
+    functionClose();
+   
+}
+iconclose.onclick=function(e){
+    functionClose();
+    e.stopPropagation();
+    console.log(1)
+}
